bd797ea55a54fc65a7dae2926fbad9068f78e9d2
FIRST_NAME LASTE <shashikant@deligence.com>
2022-08-02 11:55:21 +0530

    count

---
+++ src/Count.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+const Count = ()=>{
+    const [seconds , setSeconds ] = useState(0);
+    useEffect(()=>{
+        const timerId = setInterval(() => {
+            setSeconds(seconds+1);
+        }, 1000);
+
+        return ()=>{    
+            clearInterval(timerId);
+        }
+    }, [seconds])
+    console.log('outside code');
+    return <div>
+        {console.log('page is render')}
+        <h1>Program start seconds {seconds}</h1>
+    </div>
+}
+
+export default Count;
