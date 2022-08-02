35d561fae653e220daf8e36f8b125ece48cb5519
FIRST_NAME LASTE <shashikant@deligence.com>
2022-08-02 12:23:00 +0530

    add my name in card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Count from './Count';
+import Skk from './Skk';
 
 function App() {
   return (
     <div className="App">
       <Count/>
+      <Skk/>
     </div>
   );
 }
---
+++ src/Skk.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Skk() {
+  return (
+    <div><h1>MY NAME IS SHASHI KANT KUMAR</h1></div>
+  )
+}
+
+export default Skk
